92a0c3b8ba5bbd548096b706de1aac239bf80bd3
andreea.chivu2101 <andreea.chivu2101@fep-62-1.grid.pub.ro>
2015-12-30 18:04:08 +0200

    scripts and times

--- openmp/run.sh
+++ openmp/run.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 module load compilers/solarisstudio-12.3
-collect ./img_seg ../test/img1.pgm
+#collect ./img_seg ../test/mango_resized.pgm 4 1000
+#collect ./img_seg ../test/mango_resized.pgm 8 1000
+#collect ./img_seg ../test/mango_resized.pgm 16 1000
+#collect ./img_seg ../test/mango_resized.pgm 6 1000
+#collect ./img_seg ../test/mango_resized.pgm 12 1000
+
+#collect ./img_seg ../test/mango_resized.pgm 4 3000
+#collect ./img_seg ../test/mango_resized.pgm 8 3000
+#collect ./img_seg ../test/mango_resized.pgm 16 3000
+
+#collect ./img_seg ../test/mango_resized.pgm 4 6000
+#collect ./img_seg ../test/mango_resized.pgm 8 6000
+#collect ./img_seg ../test/mango_resized.pgm 16 6000
+
+#collect ./img_seg ../test/mango_resized.pgm 4 8906
+#collect ./img_seg ../test/mango_resized.pgm 8 6297
+#collect ./img_seg ../test/mango_resized.pgm 16 4453
+
+collect ./img_seg ../test/creioane.pgm 4 100
+#collect ./img_seg ${image} ${threads} ${chunck}
